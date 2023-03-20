ec45c1a86c7752f44309ac505a672ca9bba306d0
lutzzdias <thiagolutzdias@hotmail.com>
2023-03-20 10:19:27 -0400

    Create simple widget test

---
+++ test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:only_sales/src/features/authentication/presentation/account/account_screen.dart';
+
+void main() {
+  testWidgets(
+    'Cancel logout',
+    (tester) async {
+      await tester.pumpWidget(
+        const ProviderScope(child: MaterialApp(home: AccountScreen())),
+      );
+      final logoutButton = find.text('Logout');
+      expect(logoutButton, findsOneWidget);
+      await tester.tap(logoutButton);
+      await tester.pump();
+      final dialogTitle = find.text('Are you sure?');
+      expect(dialogTitle, findsOneWidget);
+      final cancelButton = find.text('Cancel');
+      expect(cancelButton, findsOneWidget);
+      await tester.tap(cancelButton);
+      await tester.pump();
+      expect(dialogTitle, findsNothing);
+    },
+  );
+}
