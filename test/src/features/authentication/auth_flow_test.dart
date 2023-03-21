839925c75e210a6f105c3cd0a4b25eb34efbb54e
lutzzdias <thiagolutzdias@hotmail.com>
2023-03-21 11:38:01 -0400

    Finish auth flow test

--- test/src/features/authentication/auth_flow_test.dart
+++ test/src/features/authentication/auth_flow_test.dart
@@ -4,8 +4,20 @@ import '../../robot.dart';
 
 void main() {
   testWidgets('Sign in and sign out flow', (tester) async {
+    // Sign in
     final robot = Robot(tester);
     await robot.pumpMyApp();
+    robot.expectFindAllProductCards();
+    await robot.openPopupMenu();
+    await robot.auth.openEmailPasswordSignInScreen();
+    await robot.auth.signInWithEmailAndPassword();
+    robot.expectFindAllProductCards();
+
+    // Sign out
+    await robot.openPopupMenu();
+    await robot.auth.openAccountScreen();
+    await robot.auth.tapLogoutButton();
+    await robot.auth.tapDialogLogoutButton();
     robot.expectFindAllProductCards();
   });
 }
